1533645ae6b8b797236986c60b0a4f8cce8309a0
Christian Berg <berg.christian@gmail.com>
2015-06-28 08:22:39 +0200

    [*] Miscellaneous changes

--- bash/.profile
+++ bash/.profile
@@ -1,3 +1,6 @@
 export PATH=~/bin:$PATH
 export SVDIR=~/.sv
+export RBENV_ROOT=/usr/local/var/rbenv
 alias dus='du -ksx * | sort -n'
+
+if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -15,6 +15,8 @@
                       starter-kit-eshell
                       starter-kit-bindings
                       ace-jump-mode
+                      anzu
+                      cider
                       clojure-mode
                       clojurescript-mode
                       exec-path-from-shell
@@ -43,6 +45,38 @@
 (setq org-default-notes-file "~/org/refile.org")
 (setq org-refile-targets (quote ((nil :maxlevel . 3)
                                  (org-agenda-files :maxlevel . 3))))
+(setq org-use-fast-todo-selection t)
+(setq org-agenda-span 'day)
+(setq org-todo-keywords
+      '((sequence "TODO(t!)" "STARTED(s!)" "BLOCKED(b@)" "|"
+                  "DONE(d!)" "CANCELLED(c@)")))
+(setq org-drawers (quote ("PROPERTIES" "LOGBOOK")))
+(setq org-log-into-drawer "LOGBOOK")
+
+(setq org-tag-alist '(("focus" . ?f)))
+(setq org-agenda-custom-commands
+      '(("w" "Work, focussed"
+         ((agenda "")
+          (tags "+focus-CATEGORY=\"privat\"")
+          (tags-todo "TODO=\"STARTED\"-CATEGORY=\"privat\"")))
+        ("h" "Home, focussed"
+         ((agenda "")
+          (tags "+focus-CATEGORY=\"zalando\"")
+          (tags-todo "TODO=\"STARTED\"-CATEGORY=\"zalando\"")))
+        ("r" "Refile"
+         ((tags "CATEGORY=\"refile\"")))))
+
+(add-hook 'org-mode-hook
+          (lambda () (set-input-method 'latin-postfix)))
+
+(add-hook 'org-load-hook
+          (lambda () (run-at-time t 60 'org-save-all-org-buffers)))
+
+(require 'org-crypt)
+(org-crypt-use-before-save-magic)
+(setq org-tags-exclude-from-inheritance (quote ("crypt")))
+
+(setq org-crypt-key "FA132161")
 
 (add-to-list 'auto-mode-alist '("\\.ya?ml$" . yaml-mode))
 (add-to-list 'auto-mode-alist '("\\.pp$". puppet-mode))
@@ -50,7 +84,7 @@
 (add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Vagrantfile" . ruby-mode))
 
-(add-hook 'org-mode-hook
-          (lambda () (set-input-method 'latin-postfix)))
-
-(run-at-time t 60 'org-save-all-org-buffers)
+(setq standard-indent 2
+      js-indent-level 2
+      sh-indentation 2
+      sh-basic-offset 2)
