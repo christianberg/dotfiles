e3e2a04b74300c79d2c2f21dd00a9e44e0f7d85d
Christian Berg <berg.christian@gmail.com>
2016-04-07 23:07:54 +0200

    [emacs] Set custom ledger reports

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -88,3 +88,21 @@
       js-indent-level 2
       sh-indentation 2
       sh-basic-offset 2)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ledger-reports
+   (quote
+    (("budget" "ledger -f /Users/chberg/ledger/main.dat bal Budget -X € -e 'next month'")
+     ("bal" "ledger -f %(ledger-file) bal")
+     ("reg" "ledger -f %(ledger-file) reg")
+     ("payee" "ledger -f %(ledger-file) reg @%(payee)")
+     ("account" "ledger -f %(ledger-file) reg %(account)")))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
