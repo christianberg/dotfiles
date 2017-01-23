ab82052df4beb0386a32a52d057701976e03c6d8
Christian Berg <berg.christian@gmail.com>
2017-01-23 13:34:35 +0100

    Restore Emacs config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -46,6 +46,72 @@
 (ido-ubiquitous-mode 1)
 
 (load-theme 'solarized-dark t)
+
+(exec-path-from-shell-initialize)
+
+(add-hook 'before-save-hook 'whitespace-cleanup)
+
+(global-set-key (kbd "C-c g") 'magit-status)
+
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-cb" 'org-iswitchb)
+
+(setq org-agenda-files '("~/org"))
+(setq org-default-notes-file "~/org/tasks.org")
+(setq org-refile-use-outline-path 'file)
+(setq org-refile-targets (quote ((nil :maxlevel . 3)
+                                 (org-agenda-files :maxlevel . 3))))
+(setq org-use-fast-todo-selection t)
+(setq org-agenda-span 'day)
+(setq org-todo-keywords
+      '((sequence "NEXT(n!)" "|"
+                  "DONE(d!)" "CANCELLED(c!)")))
+(setq org-drawers (quote ("PROPERTIES" "LOGBOOK")))
+(setq org-log-into-drawer "LOGBOOK")
+
+(setq org-capture-templates
+      '(("t" "Task" entry (file+datetree "~/org/tasks.org")
+         "* NEXT %?\n%i\nEntered %T\n")
+        ("j" "Journal" entry (file+datetree "~/org/journal.org")
+         "* %?\nEntered on %T\n")))
+
+(setq org-tag-alist '(("focus" . ?f)))
+(setq org-agenda-custom-commands
+      '(("w" "Work, focussed"
+         ((agenda "")
+          (tags "+focus-CATEGORY=\"privat\"")
+          (tags-todo "TODO=\"STARTED\"-CATEGORY=\"privat\"")))
+        ("h" "Home, focussed"
+         ((agenda "")
+          (tags "+focus-CATEGORY=\"zalando\"")
+          (tags-todo "TODO=\"STARTED\"-CATEGORY=\"zalando\"")))
+        ("r" "Refile"
+         ((tags "CATEGORY=\"refile\"")))))
+
+(add-hook 'org-mode-hook
+          (lambda () (set-input-method 'latin-postfix)))
+
+(add-hook 'org-load-hook
+          (lambda () (run-at-time t 60 'org-save-all-org-buffers)))
+
+(require 'org-crypt)
+(org-crypt-use-before-save-magic)
+(setq org-tags-exclude-from-inheritance (quote ("crypt")))
+
+(setq org-crypt-key "FA132161")
+
+(add-to-list 'auto-mode-alist '("\\.ya?ml$" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.pp$". puppet-mode))
+(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Vagrantfile" . ruby-mode))
+
+(setq standard-indent 2
+      js-indent-level 2
+      sh-indentation 2
+      sh-basic-offset 2)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
